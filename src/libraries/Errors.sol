eee675aa73955c9d451478df9a6dfa9bb1f60b76
vendrell46 <vendrell_46@hotmail.com>
2024-02-17 19:27:38 +0100

    merkle

---
+++ src/libraries/Errors.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+library Errors {
+    
+    // Commons
+    error AddressZero();
+    error NullAmount();
+    error InvalidParameter();
+    error SameAddress();
+    error ArraySizeMismatch();
+    error AlreadyInitialized();
+
+    // Access Control
+    error CannotBeOwner();
+    error CallerNotPendingOwner();
+    error CallerNotAllowed();
+
+    // Merkle Distributor
+    error EmptyParameters();
+    error InvalidProof();
+    error AlreadyClaimed();
+    error MerkleRootNotUpdated();
+    error EmptyMerkleRoot();
+    error IncorrectQuestID();
+    error QuestAlreadyListed();
+    error QuestNotListed();
+    error PeriodAlreadyUpdated();
+    error PeriodNotClosed();
+    error IncorrectPeriod();
+    error PeriodNotListed();
+    error TokenNotWhitelisted();
+    error IncorrectRewardAmount();
+    error CannotRecoverToken();
+
+    // HolyPalPower
+    error InvalidTimestamp();
+
+    // Vote Controller
+    error AlreadyListed();
+    error LockExpired();
+    error VotingPowerInvalid();
+    error VotingPowerExceeded();
+    error VotingPowerProxyExceeded();
+    error VotingCooldown();
+    error KilledGauge();
+    error NotKilledGauge();
+    error NotAllowedManager();
+    error NotAllowedProxyVoter();
+    error ExpiredProxy();
+    error ProxyAlreadyActive();
+    error ProxyPowerExceeded();
+    error NotAllowedVoteChange();
+
+    // Loot
+    error CreatorAlreadySet();
+    error InvalidId();
+    error VestingNotStarted();
+
+    // Loot Creator
+    error NotListed();
+
+    //Maths
+    error ConversionOverflow();
+}
---
+++ test/MultiMerkleDistributorV2Test.t.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.20;
+
+import "../src/MultiMerkleDistributorV2.sol";
+
+contract MultiMerkleDistributorV2Test {
+
+}
